c0194dd31cad9e0032f40e3f9caa6bda1d199226
Trevor John and Tyler Heck <pair+tjohn+theck@pivotallabs.com>
2013-07-22 20:17:16 -0400

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberIntro::Application.config.secret_key_base = 'a45221c01622bd9adf4d913cb4b84542ee7272962e4d8cd11e6228eae6fa50df7a91d9d51b66aef16206d8df509f671d4343ca0f0a7dd7385d5a0d6647f25290'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberIntro::Application.config.session_store :cookie_store, key: '_EmberIntro_session'
